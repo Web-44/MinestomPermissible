164c59986292af00bfff7453cc332b35050eb253
Webbiii <xdk63778x@gmail.com>
2021-04-17 17:19:01 +0200

    Update TestPermissions.java

--- src/test/java/permissions/TestPermissions.java
+++ src/test/java/permissions/TestPermissions.java
@@ -2,7 +2,6 @@ package permissions;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
-import net.minestom.server.permission.Permission;
 import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,11 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // TODO: more tests
+// TODO: no
 public class TestPermissions {
 
     private Player player;
-
-    private Permission permission1, permission2;
 
     @BeforeEach
     public void init() {
@@ -34,13 +32,12 @@ public class TestPermissions {
                 return false;
             }
         };
-
-        permission1 = new Permission("perm.name",
-                new NBTCompound()
-                        .setString("name", "Minestom")
-                        .setInt("amount", 5));
-
-        permission2 = new Permission("perm.name2");
+        player.setPermissionHandler(new PermissionHandler() {
+            @Override
+            public boolean hasPermission(String permission) {
+                return permission.equals("yes");
+            }
+        });
     }
 
     @Test
@@ -50,29 +47,11 @@ public class TestPermissions {
     }
 
     @Test
-    public void hasPermissionClass() {
-
-        assertFalse(player.hasPermission(permission1));
-        player.addPermission(permission1);
-        assertTrue(player.hasPermission(permission1));
-        assertFalse(player.hasPermission(permission2));
-
-        player.addPermission(permission2);
-        assertTrue(player.hasPermission(permission2));
-    }
-
-    @Test
-    public void hasPermissionNameNbt() {
-        player.addPermission(permission1);
-        assertTrue(player.hasPermission("perm.name"));
-        assertTrue(player.hasPermission("perm.name",
-                nbtCompound -> {
-                    final String name = nbtCompound != null ? nbtCompound.getString("name") : null;
-                    return Objects.equals(name, "Minestom");
-                }));
-
-        player.addPermission(permission2);
-        assertFalse(player.hasPermission("perm.name2", Objects::nonNull));
+    public void hasPermission() {
+        assertFalse(player.hasPermission("no"));
+        assertTrue(player.hasPermission("yes"));
+        player.setPermissionHandler(null);
+        assertFalse(player.hasPermission("yes"));
     }
 
     @AfterEach
