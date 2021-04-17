87469c5c50a67bdd3067c48da153218519d02537
Webbiii <xdk63778x@gmail.com>
2021-04-17 17:14:21 +0200

    Update ServerSender.java

--- src/main/java/net/minestom/server/command/ServerSender.java
+++ src/main/java/net/minestom/server/command/ServerSender.java
@@ -2,12 +2,6 @@ package net.minestom.server.command;
 
 import net.minestom.server.command.builder.CommandContext;
 import net.kyori.adventure.audience.Audience;
-import net.minestom.server.permission.Permission;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Sender used in {@link CommandManager#executeServerCommand(String)}.
